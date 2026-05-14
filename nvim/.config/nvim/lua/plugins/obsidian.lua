390b9f79e75853587f9bc61317176fb89444417e
Samuel He <samuelhe52@outlook.com>
2026-05-14 17:44:06 +0800

    Add obsidian.nvim

---
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "obsidian-nvim/obsidian.nvim",
+    version = "*",
+    ft = "markdown",
+    cmd = { "Obsidian" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
+}
